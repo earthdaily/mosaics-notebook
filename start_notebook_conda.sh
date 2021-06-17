dcb9cd0b4039a54edd8a13ad302e66e68010968d
rchern <rchern@urthecast.com>
2021-06-17 15:36:12 -0700

    Fix conda startup to be compatible with Docker image

--- start_notebook_conda.sh
+++ start_notebook_conda.sh
@@ -1,5 +1,5 @@
 conda init bash 
 conda env create -p $(pwd)/conda_env -f environment.yml 
 conda activate $(pwd)/conda_env
-jupyter notebook --allow-root
+jupyter notebook --allow-root --ip='*' --port=8888
 conda deactivate
